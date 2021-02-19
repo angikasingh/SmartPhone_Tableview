2457fb209960bd40c2b42141f766747f87b626d8
Angika Singh <angikasingh@Angikas-MacBook-Air.local>
2021-02-18 20:31:22 -0800

    Adding 2nd Label Temprature

--- Tableview-Xib/Tableview-Xib/Tableview-Xib/TableViewCell.swift
+++ Tableview-Xib/Tableview-Xib/Tableview-Xib/TableViewCell.swift
@@ -10,7 +10,8 @@ import UIKit
 class TableViewCell: UITableViewCell {
     
     @IBOutlet weak var lblCity: UILabel!
-
+    @IBOutlet weak var lblTemp: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- Tableview-Xib/Tableview-Xib/Tableview-Xib/TableViewController.swift
+++ Tableview-Xib/Tableview-Xib/Tableview-Xib/TableViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class TableViewController: UITableViewController {
     let cities = ["Seattle","Portland" , "SFO", "LA", "NY", "Miami"]
+    let temps = ["23°C" , "24°C", "25°C", "40°C", "34°C", "23°C"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,8 +25,7 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = Bundle.main.loadNibNamed("TableViewCell", owner: self, options: nil)?.first as! TableViewCell
         cell.lblCity.text = cities[indexPath.row]
-        print(cities)
-        print(cities[indexPath.row])
+        cell.lblTemp.text = temps[indexPath.row]
         return cell
     }
     
